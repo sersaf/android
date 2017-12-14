476834cc4d3f8fa8fa3b87fc21f84346c6774b60
Sergey Saf <sedsaf@gmail.com>
2017-12-15 00:10:17 +0300

    добавление меню

--- app/src/main/java/com/example/sergejsafonov/android_3/MainActivity.java
+++ app/src/main/java/com/example/sergejsafonov/android_3/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.sergejsafonov.android_3;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -49,5 +51,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuPurchasesListNewRecord:
+                // TODO: обработчик нажатия здесь
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 }
